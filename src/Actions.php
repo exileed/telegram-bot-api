f0878ecaad76ddf83d867dbbd94578d1ae42c459
ExileeD <me@exileed.com>
2017-11-20 23:20:40 +0100

    Add actions

--- src/Actions.php
+++ src/Actions.php
@@ -11,52 +11,33 @@ namespace Telegram\Bot;
  */
 class Actions
 {
-    /**
-     * Sets chat status as Typing.
-     *
-     * @var string
-     */
-    const TYPING = 'typing';
+	/** Sets chat status as Typing. */
+	const TYPING = 'typing';
 
-    /**
-     * Sets chat status as Sending Photo.
-     *
-     * @var string
-     */
-    const UPLOAD_PHOTO = 'upload_photo';
+	/** Sets chat status as Sending Photo. */
+	const UPLOAD_PHOTO = 'upload_photo';
 
-    /**
-     * Sets chat status as Sending Video.
-     *
-     * @var string
-     */
-    const UPLOAD_VIDEO = 'upload_video';
+	/** Sets chat status as Recording Video. */
+	const RECORD_VIDEO = 'record_video';
 
-    /**
-     * Sets chat status as Sending Audio.
-     *
-     * @var string
-     */
-    const UPLOAD_AUDIO = 'upload_audio';
+	/** Sets chat status as Sending Video. */
+	const UPLOAD_VIDEO = 'upload_video';
 
-    /**
-     * Sets chat status as Sending Document.
-     *
-     * @var string
-     */
-    const UPLOAD_DOCUMENT = 'upload_document';
+	/** Sets chat status as Recording Audio. */
+	const RECORD_AUDIO = 'record_audio';
 
-    /**
-     * Sets chat status as Choosing Geo.
-     *
-     * @var string
-     */
-    const FIND_LOCATION = 'find_location';
+	/** Sets chat status as Sending Audio. */
+	const UPLOAD_AUDIO = 'upload_audio';
 
-    /**
-     * Sets chat status as Recording Video.
-     *
-     * @var string
-     */
-    const RECORD_VIDEO = 'record_video';
+	/** Sets chat status as Sending Document. */
+	const UPLOAD_DOCUMENT = 'upload_document';
+
+	/** Sets chat status as Choosing Geo. */
+	const FIND_LOCATION = 'find_location';
+
+	/** Sets chat status as Recording Video Note. */
+	const RECORD_VIDEO_NOTE = 'record_video_note';
+
+	/** Sets chat status as Sending Video Note. */
+	const UPLOAD_VIDEO_NOTE = 'upload_video_note';
 }
